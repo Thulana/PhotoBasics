f865b3b4016f3d5aac25bc775f1e149136c60c88
thulana <thulanakannangara@gmail.com>
2017-07-19 14:27:33 +0530

    example test cases were added to check Travis

---
+++ test/UnitTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author thulana
+ */
+public class UnitTest {
+    
+    public UnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    public void exampleTest(){
+        assertEquals(2, 2);
+        
+    }
+    public void exampleTest2(){
+        assertEquals(2, 3);
+        
+    }
+    
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
